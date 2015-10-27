5155c6a7e32204e53b143f5915d5bfa0a514c2ed
Michael Klopf <klopf.michael@gmail.com>
2015-10-27 20:59:11 +0100

    Separating code in two files.

---
+++ SearchScreen.js
@@ -0,0 +1,80 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+} = React;
+
+// CMD+K for software keyboard in XCode Simulator
+var SearchScreen = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headline}>
+          Google Book Browser
+        </Text>
+        <Text style={styles.label}>
+          Find books containing:
+        </Text>
+        <TextInput
+          placeholder="e.g. Music or JavaScript"
+          returnKeyType="search"
+          enablesReturnKeyAutomatically={true}
+          onEndEditing={ event =>
+            console.dir(event.nativeEvent.text)
+          }
+          style={styles.textInput} />
+      </View>
+    );
+  }
+});
+
+// reminder: Each JSX element needs to be styled directly
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Math.random() > 0.5 ? '#CC0066' : '#0099CC'
+  },
+
+  navContainer: {
+    flex: 1
+  },
+
+  headline: {
+    fontFamily: 'Avenir Next',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#FFF',
+    marginBottom: 28
+  },
+
+  label: {
+    fontFamily: 'Avenir Next',
+    fontSize: 22,
+    fontWeight: 'normal',
+    color: '#FFF',
+    marginBottom: 8
+  },
+
+  textInput: {
+    borderColor: '#8D9996',
+    borderWidth: 0.5,
+    backgroundColor: '#FFF',
+    height: 40,
+    marginLeft: 60,
+    marginRight: 60,
+    padding: 8
+  }
+});
+
+module.exports = SearchScreen;
--- index.ios.js
+++ index.ios.js
@@ -5,11 +5,10 @@
 'use strict';
 
 var React = require('react-native');
+var SearchScreen = require('./SearchScreen');
+
 var {
   AppRegistry,
-  View,
-  Text,
-  TextInput,
   NavigatorIOS,
   StyleSheet,
 } = React;
@@ -28,68 +27,10 @@ var GoogleBookBrowser = React.createClass({
   }
 });
 
-// CMD+K for software keyboard in XCode Simulator
-var SearchScreen = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.headline}>
-          Google Book Browser
-        </Text>
-        <Text style={styles.label}>
-          Find books containing:
-        </Text>
-        <TextInput
-          placeholder="e.g. Music or JavaScript"
-          returnKeyType="search"
-          enablesReturnKeyAutomatically={true}
-          onEndEditing={ event =>
-            console.dir(event.nativeEvent.text)
-          }
-          style={styles.textInput} />
-      </View>
-    );
-  }
-});
-
 // reminder: Each JSX element needs to be styled directly
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Math.random() > 0.5 ? '#CC0066' : '#0099CC'
-  },
-
   navContainer: {
     flex: 1
-  },
-
-  headline: {
-    fontFamily: 'Avenir Next',
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#FFF',
-    marginBottom: 28
-  },
-
-  label: {
-    fontFamily: 'Avenir Next',
-    fontSize: 22,
-    fontWeight: 'normal',
-    color: '#FFF',
-    marginBottom: 8
-  },
-
-  textInput: {
-    borderColor: '#8D9996',
-    borderWidth: 0.5,
-    backgroundColor: '#FFF',
-    height: 40,
-    marginLeft: 60,
-    marginRight: 60,
-    padding: 8
   }
 });
 
