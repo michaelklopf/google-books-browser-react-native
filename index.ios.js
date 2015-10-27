3d17f3e4a6aab81a5212e4b801d5191ec7a0e0d7
Michael Klopf <klopf.michael@gmail.com>
2015-10-27 10:05:15 +0100

    Adding styling and first elements.

--- index.ios.js
+++ index.ios.js
@@ -9,6 +9,7 @@ var {
   AppRegistry,
   View,
   Text,
+  TextInput,
   StyleSheet,
 } = React;
 
@@ -16,20 +17,54 @@ var GoogleBookBrowser = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text>
-          BookBrowser
+        <Text style={styles.headline}>
+          Google Book Browser
         </Text>
+        <Text style={styles.label}>
+          Find books containing:
+        </Text>
+        <TextInput
+          placeholder="e.g. Music or JavaScript"
+          style={styles.textInput} />
       </View>
     );
   }
 });
 
+// reminder: Each JSX element needs to be styled directly
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: '#0099CC'
+  },
+
+  headline: {
+    fontFamily: 'Avenir Next',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#FFF',
+    marginBottom: 28
+  },
+
+  label: {
+    fontFamily: 'Avenir Next',
+    fontSize: 22,
+    fontWeight: 'normal',
+    color: '#FFF',
+    marginBottom: 8
+  },
+
+  textInput: {
+    borderColor: '#8D9996',
+    borderWidth: 0.5,
+    backgroundColor: '#FFF',
+    height: 40,
+    marginLeft: 60,
+    marginRight: 60,
+    padding: 8
   }
 });
 
