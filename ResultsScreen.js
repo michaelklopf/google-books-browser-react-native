43d8a6ce2b4d9c5e7b7c670465a99aaccd897640
Michael Klopf <klopf.michael@gmail.com>
2015-10-29 23:00:35 +0100

    Show search result in list view.

--- ResultsScreen.js
+++ ResultsScreen.js
@@ -4,6 +4,7 @@ var React = require('react-native');
 
 var {
   View,
+  ListView,
   Text,
   StyleSheet
 } = React;
@@ -17,7 +18,10 @@ var buildUrl = function(q) {
 var ResultsScreen = React.createClass({
   getInitialState: function() {
     return {
-      isLoading: true
+      isLoading: true,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      })
     };
   },
 
@@ -29,8 +33,11 @@ var ResultsScreen = React.createClass({
     fetch(buildUrl(searchPhrase))
       .then(response => response.json())
       .then(jsonData => {
-        this.setState({ isLoading: false });
-        console.dir(jsonData);
+        this.setState({
+          isLoading: false,
+          dataSource: this.state.dataSource.cloneWithRows(jsonData.items)
+        });
+        console.dir(jsonData.items);
       })
       .catch(error => console.dir(error));
   },
@@ -58,9 +65,22 @@ var ResultsScreen = React.createClass({
 
   renderResults: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.label}>
-          Finished searching.
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderBook}
+        style={styles.listView}
+      />
+    );
+  },
+
+  renderBook: function(book) {
+    return (
+      <View style={styles.row}>
+        <Text style={styles.title}>
+          {book.volumeInfo.title}
+        </Text>
+        <Text style={styles.subtitle}>
+          {book.volumeInfo.subtitle}
         </Text>
       </View>
     );
@@ -78,6 +98,35 @@ var styles = StyleSheet.create({
 
   label: {
     fontSize: 24,
+    fontWeight: 'normal',
+    color: '#fff'
+  },
+
+  listView: {
+
+  },
+
+  row: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5AC8FA',
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginTop: 1
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+
+  subtitle: {
+    fontSize: 16,
     fontWeight: 'normal',
     color: '#fff'
   }
